52fd2b712b04a9f461881862cd976507601f3029
Saurav kumar verma <sauravkumarverma22@gmail.com>
2024-02-18 21:48:51 +0530

    added tailwind and edited readme

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-import React from "react";
-
 const App = () => {
   return (
     <div>
-      <h1>
+      <h1 className="text-3xl font-bold underline">
         Making Worldwise App while Learning React Router DOM and Context Api
       </h1>
     </div>
