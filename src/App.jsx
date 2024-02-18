e300b2196bf6d966c067475c6067ffed9b6ae581
Saurav kumar verma <sauravkumarverma22@gmail.com>
2024-02-18 21:44:08 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1>
+        Making Worldwise App while Learning React Router DOM and Context Api
+      </h1>
+    </div>
+  );
+};
+
+export default App;
