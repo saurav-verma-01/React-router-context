d126a7bd266dedf65588e03079ff33eb5a366a30
Saurav kumar verma <sauravkumarverma22@gmail.com>
2024-02-22 17:47:33 +0530

    Map Integration is successful

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -9,11 +9,17 @@ import {
 } from "react-leaflet";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import { useCities } from "../context/CitiesContext";
+import { useGeolocation } from "../hooks/useGeoLocation";
 
 const Map = () => {
   const [mapPos, setMapPos] = useState([27.5, 77.5]);
-
   const { cities } = useCities();
+
+  const {
+    isLoading: isLoadingGeoLocation,
+    position: geolocationPostion,
+    getPosition,
+  } = useGeolocation();
 
   const [searchParams, setSearchParams] = useSearchParams();
   const mapLat = searchParams.get("lat");
@@ -23,33 +29,48 @@ const Map = () => {
     if (mapLat && mapLng) setMapPos([mapLat, mapLng]);
   }, [mapLat, mapLng]);
 
-  return (
-    <div className="h-full bg-orange-500 basis-full shrink  ">
-      <MapContainer
-        className="h-full"
-        center={mapPos}
-        zoom={6}
-        scrollWheelZoom={true}
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        {cities.map((city) => (
-          <Marker
-            position={[city.position.lat, city.position.lng]}
-            key={city.id}
-          >
-            <Popup>
-              <span>{city.emoji}</span> &nbsp; <span>{city.cityName}</span>
-            </Popup>
-          </Marker>
-        ))}
+  useEffect(() => {
+    if (geolocationPostion)
+      setMapPos([geolocationPostion.lat, geolocationPostion.lng]);
+  }, [geolocationPostion]);
 
-        <ChangeCenter position={mapPos} />
-        <DetectClick />
-      </MapContainer>
-    </div>
+  return (
+    <>
+      {geolocationPostion.lat !== mapPos[0] && (
+        <button
+          onClick={getPosition}
+          className="absolute bg-green-600 text-gray-50 p-2 rounded h-max w-max bottom-16 left-[75%] translate-x-[-50%] z-30 hover:bg-green-400 cursor-pointer "
+        >
+          {isLoadingGeoLocation ? "Loading..." : "Use your Location"}
+        </button>
+      )}
+      <div className="h-full bg-orange-500 basis-full shrink relative z-20">
+        <MapContainer
+          className="h-full"
+          center={mapPos}
+          zoom={6}
+          scrollWheelZoom={true}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          {cities.map((city) => (
+            <Marker
+              position={[city.position.lat, city.position.lng]}
+              key={city.id}
+            >
+              <Popup>
+                <span>{city.emoji}</span> &nbsp; <span>{city.cityName}</span>
+              </Popup>
+            </Marker>
+          ))}
+
+          <ChangeCenter position={mapPos} />
+          <DetectClick />
+        </MapContainer>
+      </div>
+    </>
   );
 };
 
